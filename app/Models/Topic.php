8ae692fa25834e8363c60b33d4ffec87c63ac032
PrevailExcel <prevailexcellent@gmail.com>
2025-09-16 09:35:27 +0100

    feat(topic): enable UUIDs for Topic model and set key type to string

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 
 class Topic extends Model
 {
+    use HasUuids;
+
+    public $incrementing = false;
+    protected $keyType = 'string';
+
     protected $fillable = ['user_id', 'document_id', 'topics'];
 
     protected $casts = [
