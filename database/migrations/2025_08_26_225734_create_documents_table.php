d006388e577ad2bac97286fa6f8f7d4757ab9ccd
PrevailExcel <prevailexcellent@gmail.com>
2026-01-05 22:35:07 +0100

    Removed doc id

--- database/migrations/2025_08_26_225734_create_documents_table.php
+++ database/migrations/2025_08_26_225734_create_documents_table.php
@@ -13,7 +13,6 @@ return new class extends Migration {
         Schema::create('documents', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('title')->nullable();
-            $table->string('doc_id')->unique();
             $table->string('file_type');
             $table->foreignUuid('user_id')->constrained('users');
             $table->json('metadata')->nullable();
